590e3e9047c1658b31c9882fbb8b62dd6f222657
lilacs2039 <quantum.methods@gmail.com>
2024-01-05 13:53:20 +0900

    firestore onWriteも動作することを確認

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,12 +5,12 @@ admin.initializeApp();
 const db = admin.firestore();
 
 export const createDocument = functions.https.onRequest(async (req, res) => {
-  await db.collection("test").doc("abc").set({a: 123});
+  await db.collection("test").doc("abc").set({ a: 123 });
   res.send();
 });
 
 export const updateDocument = functions.https.onRequest(async (req, res) => {
-  await db.collection("test").doc("abc").update({a: 234, b: 345});
+  await db.collection("test").doc("abc").update({ a: 234, b: 345 });
   res.send();
 });
 
@@ -23,4 +23,14 @@ export const getDocument = functions.https.onRequest(async (req, res) => {
   const data = await (await db.collection("test").doc("abc").get()).data();
   res.send(data);
 });
+
+
+export const snippet_onWrite = functions.firestore
+  .document(`test/{id}`)
+  .onWrite(async (change, context) => {
+    console.log("start snippet_onWrite");
+    const afterData = change.after.exists ? change.after.data() : null;
+    if (afterData) await admin.firestore().collection("bak").doc(change.after.id).set(afterData);
+    // if (afterData) await db.collection("bak").doc(change.after.id).set(afterData);  // これでもOK
+  });
 
